efcfd711d82058c26dc1d3ef312820714c5ca781
Jose Jacsan <jjacsan@gmail.com>
2025-06-30 21:06:49 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/josecode/cadastrodeninjas/CadastrodeninjasApplication.java
@@ -0,0 +1,13 @@
+package com.josecode.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeninjasApplication.class, args);
+	}
+
+}
